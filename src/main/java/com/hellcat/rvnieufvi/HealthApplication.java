afe700ac08b6225d684bc33dfd1bd392d357d41c
StandCN <standyangcn@gmail.com>
2017-12-13 00:32:49 +0800

    初始化

---
+++ src/main/java/com/hellcat/rvnieufvi/HealthApplication.java
@@ -0,0 +1,12 @@
+package com.hellcat.rvnieufvi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthApplication.class, args);
+	}
+}
